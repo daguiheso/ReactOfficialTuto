49bf8a089912b1e434e40612e729f7a0a51b14dd
Daniel hernandez <danielhernandez16@hotmail.com>
2016-03-05 09:42:05 -0500

    Composing components

--- bundle.js
+++ bundle.js
@@ -102,7 +102,37 @@ var CommentBox = React.createClass({
 		return React.createElement(
 			'div',
 			{ className: 'commentBox' },
-			'Hello, world! I am a CommentBox.'
+			React.createElement(
+				'h1',
+				null,
+				'Comments'
+			),
+			React.createElement(CommentList, null),
+			React.createElement(CommentForm, null)
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	displayName: 'CommentList',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			{ className: 'commentList' },
+			'Hello, world! I am a CommentList.'
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	displayName: 'CommentForm',
+
+	render: function () {
+		return React.createElement(
+			'div',
+			{ className: 'commentForm' },
+			'Hello, world! I am a CommentForm.'
 		);
 	}
 });
