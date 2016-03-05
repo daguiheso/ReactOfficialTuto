243e92c5d5bc60d2e61d666e74e78521b9e39fd0
Daniel hernandez <danielhernandez16@hotmail.com>
2016-03-05 14:08:58 -0500

    Using props

--- bundle.js
+++ bundle.js
@@ -137,6 +137,24 @@ var CommentForm = React.createClass({
 	}
 });
 
+var Comment = React.createClass({
+	displayName: 'Comment',
+
+
+	render: function () {
+		return React.createElement(
+			'div',
+			{ className: 'comment' },
+			React.createElement(
+				'h2',
+				{ className: 'commentAuthor' },
+				this.props.author
+			),
+			this.props.children
+		);
+	}
+});
+
 ReactDOM.render(React.createElement(CommentBox, null), document.getElementById('content'));
 
 },{"react":159,"react-dom":3}],3:[function(require,module,exports){
